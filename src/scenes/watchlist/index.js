c4cc703f28ef9389a46d299beea58b1fed64aac1
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-01-05 13:30:51 +0100

    Added placeholder container for watchlists

---
+++ src/scenes/watchlist/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class WatchlistContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Watchlist</h1>
+            </div>
+        )
+    }
+}
